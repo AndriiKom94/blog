7ea93907286c727cc23dad215295ea1c2a50729d
AndriiKom94 <andrii.kom@ukr.net>
2021-01-14 00:23:31 +0200

    Add events/home.html

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateField()
-    text = models.CharField(max_length=300)
+    date = models.DateTimeField()
+    text = models.TextField()
     post_image = models.ImageField(upload_to='post_images/')
